99ff5cbae2452deab62f3bd4fd72bb4e174362bb
Nuno Maduro <enunomaduro@gmail.com>
2019-09-06 17:57:00 +0200

    feat: adds first readme

---
+++ readme.php
@@ -0,0 +1,27 @@
+---
+home: true
+title: Plus
+description: Plus is a superset of PHP that makes PHP cool again
+heroImage: /logo.gif
+actionText: Get Started →
+actionLink: /get-started
+sidebar: true
+features:
+    - title: State of the art PHP
+      details: ...
+    - title: Types are optional
+      details: ...
+    - title: Zero fuss
+      details: ....
+footer: MIT Licensed | Copyright © Nuno Maduro
+---
+
+# Installation
+
+```bash
+# First, install:
+composer require php-plus/engine
+
+# To use the Plus syntax add the following declare:
+declare(plus=1);
+```
